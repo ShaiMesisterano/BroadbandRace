658ef6f56c1ebb2cea3409fc4c55cf086b173a7f
Shai Mesisterano <shai@mcteano.com>
2012-11-11 18:38:20 +0200

    Create Elements
    1. Create Flags
    2. Create Cars

--- js/script.js
+++ js/script.js
@@ -13,36 +13,93 @@ var FlagCollection = Backbone.Collection.extend({
 
 /* Views */
 var CarView = Backbone.View.extend({
-
+	className: "car",
+	template : _.template($('#car_template').html()),
+	createCar : function() {
+		// create Car Element
+		var carModel = this.model;
+		var element = this.getElement(carModel);
+		this.driveCar();
+		return element;
+	},
+	getElement : function(carModel) {
+		// append in the current template
+		var carElement = this.template(carModel.toJSON());
+		$(this.el).append(carElement);
+		return this;
+	},
+	driveCar: function(){
+		// increase car's position by its speed value
+		var carSpeed = this.model.get("speed");
+		var _this = this;
+		setInterval(function(){
+			var currentLeft = $(_this.el).position().left;
+			var newLeft = currentLeft + carSpeed;
+			$(_this.el).css('left', newLeft + 'px');
+			
+			if (_this.model.get("id") == "6"){
+				$("#road").css('left', newLeft + 'px');
+			}
+			
+		}, 1000);
+	}
+});
+var CarsView = Backbone.View.extend({
+	el : '#cars',
+	initialize : function() {
+		// populate models and create views with them
+		var carsModels = this.collection.models;
+		this.createCars(carsModels);
+	},
+	createCars : function(carsModels) {
+		// loop through flags data and create their animations
+		var _this = this;
+		_.each(carsModels, function(carModel) {
+			var carView = new CarView({
+				model : carModel
+			});
+			$(_this.el).append(carView.createCar().el);
+		});
+	}
 });
 var FlagsView = Backbone.View.extend({
 	el : '#flags',
-	template: _.template($('#flags_template').html()),
-	events : {
-		"click" : "selectFlag"	//select flag for racing
-	},
 	initialize : function() {
+		// populate models and create views with them
 		var flagsModels = this.collection.models;
 		this.createFlags(flagsModels);
 	},
 	createFlags : function(flagsModels) {
+		// loop through flags data and create their animations
 		var _this = this;
-		//Loop through flags data and create their animations
 		_.each(flagsModels, function(flagModel) {
-			_this.createFlag(flagModel);
+			var flagView = new FlagView({
+				model : flagModel
+			});
+			$(_this.el).append(flagView.createFlag().el);
 		});
+	}
+});
+var FlagView = Backbone.View.extend({
+	tagName : "li",
+	className : "flag",
+	template : _.template($('#flag_template').html()),
+	events : {
+		"click" : "toggleFlag"	//select/unselect flag for racing
 	},
-	createFlag : function(flagModel) {
+	createFlag : function() {
+		var flagModel = this.model;
 		this.initRequestAnimFrame();
+		// animation settings
 		var country_id = flagModel.get("id");
 		var img_src = "images/flag/" + country_id + ".gif";
 		var angularSpeed = 2;
 		var lastTime = 0;
 		var renderer = this.getRenderer(100, 100);
-		this.appendRenderer(renderer, flagModel);
 		var camera = this.getCamera(35, 1, 100, 1000, 700);
 		var material = this.getMaterial(img_src);
 		var sphere = this.getSphere(material);
+		// add scene
 		var scene = new THREE.Scene();
 		scene.add(sphere);
 		// add subtle ambient lighting
@@ -51,25 +108,30 @@ var FlagsView = Backbone.View.extend({
 		// add directional light source
 		var directionalLight = this.getDirectionalLight(0xffffff, 1, 1, 1);
 		scene.add(directionalLight);
-		// create wrapper object that contains three.js objects
+		// add wrapper object that contains three.js objects
 		var three = {
 			renderer : renderer,
 			camera : camera,
 			scene : scene,
 			sphere : sphere
 		};
+		// add texture
 		this.createTexture(lastTime, angularSpeed, three, img_src);
+		// return appended element back to FlagsView
+		var element = this.getElement(renderer, flagModel);
+		return element;
 	},
-	getRenderer: function(width, height){
+	getRenderer : function(width, height) {
 		var renderer = new THREE.WebGLRenderer();
 		renderer.setSize(width, height);
 		return renderer;
 	},
-	appendRenderer: function(renderer, flagModel){
-		// append in current element
-		$(this.el).append(renderer.domElement);
+	getElement : function(renderer, flagModel) {
+		// append element in the current template
 		var flagElement = this.template(flagModel.toJSON());
-		$(this.el).append(flagElement);
+		$(this.el).append(flagElement + "<br />");
+		$(this.el).append(renderer.domElement);
+		return this;
 	},
 	getCamera : function(view_angle, aspect, near, far, z_position) {
 		var camera = new THREE.PerspectiveCamera(view_angle, aspect, near, far);
@@ -81,7 +143,7 @@ var FlagsView = Backbone.View.extend({
 		sphere.overdraw = true;
 		return sphere;
 	},
-	getSphereGeometry: function(radius, segments, rings){
+	getSphereGeometry : function(radius, segments, rings) {
 		var sphereGeometry = new THREE.SphereGeometry(radius, segments, rings);
 		return sphereGeometry;
 	},
@@ -100,7 +162,7 @@ var FlagsView = Backbone.View.extend({
 		directionalLight.position.set(x_position, y_position, z_position).normalize();
 		return directionalLight;
 	},
-	createTexture: function(lastTime, angularSpeed, three, img_src){
+	createTexture : function(lastTime, angularSpeed, three, img_src) {
 		// wait for texture image to load before starting the animation
 		var _this = this;
 		var textureImg = new Image();
@@ -113,12 +175,13 @@ var FlagsView = Backbone.View.extend({
 		var date = new Date();
 		var time = date.getTime();
 		var timeDiff = time - lastTime;
+		// Calculate angle change and rotate it
 		var angleChange = angularSpeed * timeDiff * 2 * Math.PI / 10000;
 		three.sphere.rotation.y += angleChange;
 		lastTime = time;
 		// render animation
 		three.renderer.render(three.scene, three.camera);
-		// restart animation
+		// set recursive animation
 		var _this = this;
 		requestAnimFrame(function() {
 			_this.animateFlag(lastTime, angularSpeed, three);
@@ -133,9 +196,9 @@ var FlagsView = Backbone.View.extend({
 			};
 		})();
 	},
-	selectFlag : function() {
-		
-	},
+	toggleFlag : function() {
+		$(this.el).css('background', 'green');
+	}
 });
 
 /* Router */
@@ -144,10 +207,12 @@ var AppRouter = Backbone.Router.extend({
 		"*actions" : "load" /* Default Route */
 	},
 	load : function() {
-		this.loadFlags(); /* Load flags from JSON data */
+		// this.loadFlags(); /* Load flags from JSON data */ /**/
+		this.loadCars();
 	},
 	loadFlags : function() {
 		var flagCollection = new FlagCollection();
+		// fetch data from JSON
 		flagCollection.fetch({
 			success : function(data) {
 				var flagsView = new FlagsView({
@@ -158,6 +223,50 @@ var AppRouter = Backbone.Router.extend({
 			error : function(response) {
 				console.log("Error occured while accessing JSON Data. Response: ", response);
 			}
+		});
+	},
+	loadCars : function() {
+		// create 6 cars collection
+		var carCollection = new CarCollection([{
+			id : "1",
+			top : "0",
+			left : "10",
+			color : "red",
+			speed: 149.616
+		}, {
+			id : "2",
+			top : "100",
+			left : "10",
+			color : "orange",
+			speed: 101.807
+		}, {
+			id : "3",
+			top : "200",
+			left : "10",
+			color : "blue",
+			speed: 72.003
+		}, {
+			id : "4",
+			top : "300",
+			left : "10",
+			color : "green",
+			speed: 71.866
+		}, {
+			id : "5",
+			top : "400",
+			left : "10",
+			color : "yellow",
+			speed: 69.973
+		}, {
+			id : "6",
+			top : "500",
+			left : "10",
+			color : "purple",
+			speed: 57.552
+		}]);
+		// create a view for the cars
+		var carsView = new CarsView({
+			collection : carCollection
 		});
 	}
 });
